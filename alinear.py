ac8fdd98ee157a82d13268225612ff6003f5b771
Mauricio Araya <mauricio@mac.local>
2015-04-14 21:10:58 -0300

    Software licensed

--- alinear.py
+++ alinear.py
@@ -1,3 +1,21 @@
+"""
+This file is part of ChiVO
+Copyright (C) José Castro, Marco Salinas
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
 import numpy as np
 
 def alinear(x1,y1,x2,y2,dim_1_x,dim_1_y,dim_2_x,dim_2_y, actual): # 1: Referencia 2: Pos. de la imagen actual
